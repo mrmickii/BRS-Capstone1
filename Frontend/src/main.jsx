59d9c096922ac03547c69a69726870726dbe27ac
Carlo R. Garcia <garciaram.cal@gmail.com>
2024-04-01 18:47:27 +0800

    updating reservation

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-//import Login from '../src/Components/login'
+import Login from './Components/userView/login'
 import Reservation2 from './Components/userView/reservation2'
-import Reservation2 from './Components/userView/reservation2'
-// import Notification from './Components/userView/notification'
-// import HeadView from './Components/headView/headview';
+import Notification from './Components/userView/notification'
+import HeadView from './Components/headView/headSide';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Reservation2/> */}
+    <Notification/>
     {/* <HeadView /> */}
-    <Reservation2 />
+    {/* <Reservation2 /> */}
   </React.StrictMode>,
 )
