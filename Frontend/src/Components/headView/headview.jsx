e1d35d46b177839c80c20bc86ab02a2dfe81db3f
Carlo R. Garcia <garciaram.cal@gmail.com>
2024-04-01 18:16:44 +0800

    changes in file paths

---
+++ Frontend/src/Components/headView/headview.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../../CSS/headCSS/headview.css'
+import Header from '../userView/header'
+import SideNavBar from '../userView/sidenavbar'
+
+const HeadView = () => {
+  return(
+    <>
+      <Header />
+      <SideNavBar />
+    </>
+  );
+}
+
+export default HeadView
---
+++ Frontend/src/Components/userView/login.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { FaUser, FaLock } from 'react-icons/fa';
+import '../CSS/login.css';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    if (username === 'user' && password === 'password') {
+      setLoggedIn(true);
+    } else {
+      alert('Invalid credentials. Please try again.');
+    }
+  };
+
+  const handleClear = () => {
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <div className='main'>
+      <div className='citlogo'></div>
+     
+      <div className='title'>
+        <h1>TRANSPORTATION RESERVATION SYSTEM</h1>
+      </div>
+      <div className='login-container'>
+        <h2>User Authentication</h2>
+        {isLoggedIn ? (
+          <p>You are logged in!</p>
+        ) : (
+          <form>
+            <label>
+              <FaUser />
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+              />
+            </label>
+            <label>
+              <FaLock />
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
+            </label>
+            <button className='login-btn' type="button" onClick={handleLogin}>
+              LOGIN
+            </button>
+            <button className='clear-btn' type="button" onClick={handleClear}>
+              CLEAR ENTITIES
+            </button>
+            <div className='fpass'>
+              <p>Forgot Password? <a href="#">Click here</a></p>
+            </div>
+          </form>
+        )}
+      </div>
+      <div className='cit-bglogo'></div>
+    </div>
+  );
+};
+
+export default Login;
