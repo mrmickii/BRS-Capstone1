d61d18a42f8895a62494184f9c9e4f0065537c1f
Rise Jade Benavente <risejade.benavente@cit.edu>
2024-04-01 21:54:24 +0800

    OPC side Creatation

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,12 +4,14 @@ import Login from './Components/userView/login'
 import Reservation2 from './Components/userView/reservation2'
 import Notification from './Components/userView/notification'
 import HeadView from './Components/headView/headSide';
+import OpcView from './Components/opcView/opcSide'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/*<Notification/>*/}
    {/* <Login />*/}
     {/* <HeadView /> */}
-    <Reservation2 /> 
+     <OpcView /> 
+    {/*<Reservation2 /> */}
   </React.StrictMode>,
 )
