7c0378a4a0f298434b036576a97d74e5a81739a6
Carlo R. Garcia <garciaram.cal@gmail.com>
2024-04-01 16:58:19 +0800

    updated file paths

---
+++ Frontend/src/Components/headView/headSide.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import '../../CSS/headCSS/headSide.css'
+import Header from '../userView/header'
+import SideNavBar from '../userView/sidenavbar'
+
+const HeadSide = () =>{
+  return(
+    <div className="head-view-container">
+      <Header />
+      <SideNavBar />
+      <div className="head-view-content-container">
+        <div className="head-view-content">
+
+          <div className="banner"></div>
+
+          <div className="l-request">
+            <h1>LIST OF REQUESTS</h1>
+            <div className="department-selector">
+              <p>Select Department:</p>
+              <select name="selector" id="selector">
+                <option value="0">--Choose a Department--</option>
+                <option value="1">Collage of Computer Studies (CCS) </option>
+                <option value="1">Collage of Nursing and Allied Health Sciences(CNAHS) </option>
+              </select>
+              <button>Set Configuration</button>
+            </div>
+          </div>
+
+          <div className="r-content">
+            {/* DIRI MASULOD ANG CONTENT IF APPROVE BA OR DILI */}
+          </div>
+        </div>
+      </div>
+      <div className="bg-logo"></div>
+    </div>
+  );
+}
+
+export default HeadSide
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-//import Login from '../src/Components/login'
-import Reservation2 from './Components/userView/reservation2'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Components/userView/login'
+// import Reservation2 from './Components/userView/reservation2'
+// import HeadSide from './Components/headView/headSide';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Reservation2/>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Login />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
