4728701673cca8a5f404aae79d97893081865b0a
Carlo R. Garcia <garciaram.cal@gmail.com>
2024-04-01 23:56:43 +0800

    Added reservation API

---
+++ Backend/brscapstone1/src/main/java/com/brscapstone1/brscapstone1/Repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.brscapstone1.brscapstone1.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brscapstone1.brscapstone1.Entity.ReservationEntity;
+
+public interface ReservationRepository extends JpaRepository<ReservationEntity, Integer>{
+
+}
