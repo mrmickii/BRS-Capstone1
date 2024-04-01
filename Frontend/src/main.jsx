6928abae704e72bd97cfa05dda8bd6c199d94bc1
Carlo R. Garcia <garciaram.cal@gmail.com>
2024-04-02 00:25:50 +0800

    updated the logo

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -14,7 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <Login /> */}
     {/* <HeadView /> */}
      {/* <OpcView />  */}
-    {/*<Reservation2 /> */}
     <Reservation2 /> 
     {/* <Reservation />  */}
     {/* <Settings /> */}
