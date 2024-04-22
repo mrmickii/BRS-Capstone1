8a69532ae5f8002572f8d36201182405e174229a
Rise Jade Benavente <risejade.benavente@cit.edu>
2024-04-22 12:14:26 +0800

    opc side update button

--- Frontend/src/Components/opcView/Modal.jsx
+++ Frontend/src/Components/opcView/Modal.jsx
@@ -1,61 +1,48 @@
 import React from 'react';
 import '../../CSS/opcCSS/modal.css'
 
-function Modal({ onClose }){
+function Modal({ onClose }) {
 
   const Button = ({ onClick, children }) => (
     <button onClick={onClick}>{children}</button>
   )
 
-    return(
-        <div className="modalBackground">
-          <div className="modalContainer">
-            <Button onClick={onClose}> X </Button>
-            <div className="modaltitle">
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+          <button className="closeButton" onClick={onClose}>X</button>
+            <div className="modalHeader">
               <h1>UPDATE REQUEST</h1>
-            </div>
-            <div className="subtitle">
-              <p>EDUCATIONAL TOUR</p>
+              <p className="subtitle">EDUCATIONAL TOUR</p>
             </div>
             <form>
-            <div className="body1-1">
-              <input placeholder="Type of Trip"/>
-            </div>
-            <div className="body1-2">
-              <input placeholder="From"/> 
-            </div>
-            <div className="body1-3">
-              <input placeholder="To"/> 
-            </div>
-            <div className="body1-4">
-              <input placeholder="Date"/>
-            </div>
-            <div className="body2-1">
-              <input placeholder="Department"/>
-            </div>
-            <div className="body2-2">
-              <input placeholder="Send Time"/>
-            </div>
-            <div className="body2-3">
-              <input placeholder="Pick Up Time"/>
-            </div>
-            <div className="body3-1">
-              <input placeholder="Type of Trip"/>
-            </div>
-            <div className="body3-2">
-              <input placeholder="Vehicle"/>
-            </div>
-            <div className="body3-3">
-              <input placeholder="Capacity"/>
-            </div>
-            <div className="body4-1">
-              <input placeholder="Reason of Trip"/>
-            </div>
-            <button>SAVE CHANGES</button>
-            </form>
-          </div>
+                <div className="formRow grid grid-cols-4 gap-4">
+                  <input placeholder="Type of Trip"/>
+                  <input placeholder="From"/> 
+                  <input placeholder="To"/> 
+                  <input placeholder="Date"/>
+                </div>
+                <div className="formRow grid grid-cols-3 gap-4">
+                  <input placeholder="Department"/>
+                  <input placeholder="Send Time"/>
+                  <input placeholder="Pick-Up Time"/>
+                </div>
+                <div className="formRow grid grid-cols-4 gap-4">
+                  <input placeholder="Pick-up point"/>
+                  <input placeholder="Drop-off point"/>
+                  <input placeholder="Vehicle"/>
+                  <input placeholder="Capacity"/>
+                </div>
+                <div className="formRow">
+                  <input placeholder="Reason of Trip"/>
+                </div>
+                <div className="opc-save-container">
+                  <button className='opc-save-button'>SAVE CHANGES</button>
+                </div>
+              </form>
         </div>
-    )
+    </div>
+  )
 }
 
 export default Modal;
