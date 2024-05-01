7ff462d8c2ee336b51fc464afb6251b72e1223f4
Rise Jade Benavente <risejade.benavente@cit.edu>
2024-05-01 22:53:16 +0800

    opc side with driver and vehicle

---
+++ Frontend/src/Components/opcView/opcVehicle.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../userView/header';
+import SideNavBar from '../opcView/sidenavbar';
+
+const OpcVehicle = () => {
+  return (
+    <div>
+      <Header />
+      <SideNavBar />
+      {/* Your opcDriver content goes here */}
+    </div>
+  );
+}
+
+export default OpcVehicle;
