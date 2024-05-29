0167c7784a55400027f89c54ae87c0dc397c68a7
KmvrlyBcls <kimverly.bacalso@cit.edu>
2024-05-29 20:44:06 +0800

    updateDriver bug

--- Frontend/src/Components/opcView/OpcUpdateDriver.jsx
+++ Frontend/src/Components/opcView/OpcUpdateDriver.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../CSS/opcCSS/opc-update-driver.css';
 
 const UpdateDriver = ({ driver, onUpdate, onClose }) => {
   const [updatedDriverName, setUpdatedDriverName] = useState(driver.driverName);
   const [updatedContactNumber, setUpdatedContactNumber] = useState(driver.contactNumber);
-  const [updatedStatus, setUpdatedStatus] = useState(driver.contactNumber);
+  const [updatedStatus, setUpdatedStatus] = useState(driver.status); // Corrected state initialization
   const [successMessage, setSuccessMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSuccessMessage('');
+    }, 3000); // Clear success message after 3 seconds
+    return () => clearTimeout(timer);
+  }, [successMessage]);
 
   const handleUpdateDriver = async () => {
     try {
